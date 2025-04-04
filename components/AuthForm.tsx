9fe590ac56bd94626d268691d71a998852c69abd
Kalar2025 <kalarsu@gmail.com>
2025-04-04 16:07:41 -0700

    update sign-in/sign-up UI

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -86,11 +86,15 @@ const AuthForm = ({type}: {type:FormType}) => {
         <div className="card-border lg:min-w-[560px]">
             <div className="flex flex-col gap-6 card py-14 px-10">
                 <div className="flex flex-row gap-2 justify-center">
-                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
-                    <h2 className="text-primary-100">PrepWise</h2>
+                    <Image src="/logo.png" alt="logo" height={32} width={38} />
+                    <h2 className="text-blue-500">Dev Crack AI</h2>
                 </div>
 
-                <h3>Practice job interview with AI</h3>
+                <h3 className="text-center capitalize">
+                    { type === "sign-in"
+                        ? "Please sign in to see your AI-generated Interview"
+                        : "Please sign up to create your AI-generated Interview"
+                    }</h3>
 
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full mt-4 form">
